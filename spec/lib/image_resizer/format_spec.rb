a9adae2366d70019e7987d60aee20dd1b1471652
Paolo Zaccagnini <paolo@pzac.net>
2014-11-05 17:07:18 +0000

    Spec clean-up

--- spec/lib/image_resizer/format_spec.rb
+++ spec/lib/image_resizer/format_spec.rb
@@ -4,41 +4,42 @@ module ImageResizer
   describe Format do
     describe 'operations' do
       let(:output) { subject.to_s }
+      let(:format) { described_class.new }
 
       describe '#optimize' do
-        subject { described_class.new.optimize(quality: 80) }
+        subject { format.optimize(quality: 80) }
         specify { expect(output).to eq 'O=80'}
       end
 
       describe '#aspect_ratio' do
-        subject { described_class.new.aspect_ratio('16x9') }
+        subject { format.aspect_ratio('16x9') }
         specify { expect(output).to eq 'C=AR16x9' }
       end
 
       describe '#square_crop' do
-        subject { described_class.new.square_crop }
+        subject { format.square_crop }
         specify { expect(output).to eq 'C=SQ' }
       end
 
       describe '#resize' do
-        subject { described_class.new.resize(width: 80, height: 70,
+        subject { format.resize(width: 80, height: 70,
           upscale: true) }
         specify { expect(output).to eq 'S=W80,H70,U' }
       end
 
       describe '#crop' do
-        subject { described_class.new.crop(width: 80, height: 70,
+        subject { format.crop(width: 80, height: 70,
           x_offset: 10, y_offset: 20) }
         specify { expect(output).to eq 'C=W80,H70,X10,Y20' }
       end
 
       describe '#resize_wxh' do
-        subject { described_class.new.resize_wxh('1024x768') }
+        subject { format.resize_wxh('1024x768') }
         specify { expect(output).to eq 'S=W1024,H768' }
       end
 
       describe '#crop_from_jcrop' do
-        subject { described_class.new.crop_from_jcrop('10:20;30,40') }
+        subject { format.crop_from_jcrop('10:20;30,40') }
         it 'parses the custom string correctly' do
           expect(output).to eq 'C=W10,H20,X30,Y40'
         end
